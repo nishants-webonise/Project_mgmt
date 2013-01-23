86f0920c7f82f6457a9befd7174e8a988a72e434
Nishant Samel <nishants@weboniselab.com>
2013-01-23 16:16:00 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectManagementSystem::Application.config.secret_token = '19d34c09c5c182ad836833828747eb12885dd6c2d1a6d84f392f6095a936ef783659edfb1f3e45f3a4f68a68a8110c8af68fe8885b786644a571b9a2d1da9e11'
